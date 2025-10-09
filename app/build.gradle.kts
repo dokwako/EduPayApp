f6c826e08c6840e8bc764fdf7ce85c6e82490516
dokwako <denzilwawire418@gmail.com>
2025-10-09 23:47:13 +0300

    added new dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,5 +58,7 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     implementation("androidx.core:core-splashscreen:1.0.1")
 
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+
 
 }
