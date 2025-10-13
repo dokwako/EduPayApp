7043e61a84a9704c20d0715e9b8b18e9960d9fd3
dokwako <denzilwawire418@gmail.com>
2025-10-13 22:07:14 +0300

    improved

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,8 +57,13 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     implementation("androidx.core:core-splashscreen:1.0.1")
-
+     //navigation
     implementation("androidx.navigation:navigation-compose:2.7.7")
+
+    //view model
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.2")
 
 
 }
