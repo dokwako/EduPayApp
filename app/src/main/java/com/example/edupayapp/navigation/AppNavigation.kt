a2d31468d92cbeeb888ff8fb92ecb3f52c99681c
dokwako <denzilwawire418@gmail.com>
2025-10-19 23:56:29 +0300

    new

--- app/src/main/java/com/example/edupayapp/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/edupayapp/navigation/AppNavigation.kt
@@ -18,6 +18,7 @@ fun AppNavigation() {
     val navController = rememberNavController()
 
     //nav host - container for all screens
+    //
     NavHost(
         navController = navController,
         startDestination = "welcome"
