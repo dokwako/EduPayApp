ed48e1cdda4ec624be112430d5f5662e3fae2951
dokwako <denzilwawire418@gmail.com>
2025-11-10 23:57:20 +0300

    new

--- app/src/main/java/com/example/edupayapp/MainActivity.kt
+++ app/src/main/java/com/example/edupayapp/MainActivity.kt
@@ -4,11 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.edupayapp.di.AppModule
 import com.example.edupayapp.navigation.AppNavigation
 import com.example.edupayapp.ui.theme.EduPayAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        // ✅ Initialize Supabase ONCE with context
+        AppModule.initSupabase(this)
 
         super.onCreate(savedInstanceState)
         setContent {
