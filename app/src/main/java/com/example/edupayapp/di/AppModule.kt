4ef2aa9bfda59f3bf787148beacd94f3a0efd0a0
dokwako <denzilwawire418@gmail.com>
2025-11-11 23:30:51 +0300

    new changes

--- app/src/main/java/com/example/edupayapp/di/AppModule.kt
+++ app/src/main/java/com/example/edupayapp/di/AppModule.kt
@@ -1,10 +1,12 @@
 package com.example.edupayapp.di
 
 import android.content.Context
-import io.github.jan.supabase.createSupabaseClient
 import io.github.jan.supabase.auth.Auth
-import io.github.jan.supabase.auth.session.SharedPreferencesSessionManager
+import io.github.jan.supabase.createSupabaseClient
+import io.github.jan.supabase.auth.status.SessionSource
+
 import io.github.jan.supabase.postgrest.Postgrest
+import io.github.jan.supabase.storage.Storage
 
 object AppModule {
 
@@ -14,12 +16,11 @@ object AppModule {
     fun initSupabase(context: Context) {
         supabase = createSupabaseClient(
             supabaseUrl = "https://erkqutbpbyqtnzasmdhj.supabase.co",
-            supabaseKey = "your_anon_key_here"
+            supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVya3F1dGJwYnlxdG56YXNtZGhqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE4MTU4MTIsImV4cCI6MjA3NzM5MTgxMn0.R4p4-mesQFz2pBkETDCsQixet0Ks1MMjkpC5dZ66RVQ"
         ) {
-            install(Auth) {
-                sessionManager = SharedPreferencesSessionManager(context)
-            }
+            install(Auth)
             install(Postgrest)
+            install(Storage)
         }
     }
 }
