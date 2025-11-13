0e16107fdf90261acfdec82c538e3a5ded6e656e
dokwako <denzilwawire418@gmail.com>
2025-11-13 23:56:54 +0300

    new changes for error check

--- app/src/main/java/com/example/edupayapp/MainActivity.kt
+++ app/src/main/java/com/example/edupayapp/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import com.example.edupayapp.di.AppModule
+import com.example.edupayapp.data.supabase.AppModule
 import com.example.edupayapp.navigation.AppNavigation
 import com.example.edupayapp.ui.theme.EduPayAppTheme
 
